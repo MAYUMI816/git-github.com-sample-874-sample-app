77d73216673bf79c806c5e9cab4a5cfb3ae4773e
MAYUMI <cova076@gmail.com>
2019-12-03 03:53:18 +0000

    C7終了

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,62 @@
 class TasksController < ApplicationController
+  before_action :set_user #Controllerで使う同じコードはbefore_actionでまとめてしまえばいい。
+  before_action :set_task, only: %i(show edit update destroy)
+  
+  
+  
   def index
+    @user = User.find(params[:user_id])
+    @task = @user.tasks.all
+    #@task = @user.tasks
   end
-end
+  
+  def new
+    @task = Task.new
+  end
+  
+
+
+  def create
+    @task = @user.tasks.build(task_params)
+    if @task.save
+      flash[:success] = "タスクを新規作成しました。"
+      redirect_to user_tasks_url @user   #コントローラーのアクション内で自動的にページに切り替えるためのメソッド
+    else
+      render :new
+    end
+  end
+
+
+
+  def show
+  end
+  
+  def edit  
+  end
+  
+  def update
+
+      
+  end
+  
+  def destroy
+  end
+  
+  private
+
+      def task_params
+      params.require(:task).permit(:task_name, :task_detail, :user_id)
+      end
+  
+      def set_user
+        @user = User.find(params[:user_id])
+      end
+      
+      def set_task
+            unless @task = @user.tasks.find_by(id: params[:id])
+        flash[:danger] = "権限がありません。"
+        redirect_to user_tasks_url @user
+      end
+      end
+      
+end  
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-
-  get 'tasks/new'
-
+  
   root 'static_pages#top'
   get '/signup', to: 'users#new'
   
   get    '/login', to: 'sessions#new'
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  
   
   resources :users do
     resources :tasks
