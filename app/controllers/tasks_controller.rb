3f56f445e8d0151cb5fee4aad5a16198caa00a35
MAYUMI <cova076@gmail.com>
2019-12-06 12:17:30 +0000

    C１２一応完了

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,19 +4,19 @@ class TasksController < ApplicationController
   
   
   
-  def index
+  def index #* index（一覧画面）
     @user = User.find(params[:user_id])
     @task = @user.tasks.all
     #@task = @user.tasks
   end
   
-  def new
+  def new #* new（投稿の新規作成画面）
     @task = Task.new
   end
   
 
 
-  def create
+  def create #* create（投稿の新規保存）
     @task = @user.tasks.build(task_params)
     if @task.save
       flash[:success] = "タスクを新規作成しました。"
@@ -28,18 +28,27 @@ class TasksController < ApplicationController
 
 
 
-  def show
+  def show #* show（特定の投稿を表示する画面) 詳細ページ
+    @task = Task.find(params[:id])
   end
   
-  def edit  
+  def edit #* edit（投稿の編集画面）
+    @task = Task.find(params[:id])
+    @user = User.find(session[:user_id])
   end
   
-  def update
-
-      
+  def update #* update（投稿の更新保存）
+    @task = Task.find(params[:id])
+    flash[:success] = "タスクを更新しました。"
+    @task.save
+    redirect_to user_task_url @user
+ 
   end
   
-  def destroy
+  def destroy #* destroy（投稿の削除）
+    @task.destroy
+    flash[:success] = "タスクを削除しました。"
+    redirect_to user_tasks_url @user #管理者のタスク一覧(user_tasks GET    /users/:user_id/tasks(.:format)          tasks#index)
   end
   
   private
@@ -53,10 +62,10 @@ class TasksController < ApplicationController
       end
       
       def set_task
-            unless @task = @user.tasks.find_by(id: params[:id])
+        unless @task = @user.tasks.find_by(id: params[:id])
         flash[:danger] = "権限がありません。"
         redirect_to user_tasks_url @user
-      end
+        end
       end
       
 end  
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
+#4. 4. 1 存在性の検証
+  validates :name, presence: true, length: { maximum: 50 } #name属性に50文字以下であること#
+  validates :description, presence: true, length: { in: 5..300 }
 end
