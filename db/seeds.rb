c943d478d818577d09b0e40f2ea842e33325d9cd
MAYUMI <cova076@gmail.com>
2020-02-25 06:34:29 +0000

    D5 ユーザーの一覧ページを作成する

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,10 @@ User.create!( name: "Sample User",
                password: password,
                password_confirmation: password)
 end
+# D7 タスクをサンプルとして自動生成する2 ~ 3名のユーザーを対象に、各50タスク程度
+@users = User.order(:created_at).take(3)
+50.times do |n|
+  task_name =Faker::Lorem.sentence(2)
+  task_description = Faker::Lorem.sentence(5)
+  @users.each { |user| user.tasks.create!(task_name: task_name, task_description: task_description) }
+end
